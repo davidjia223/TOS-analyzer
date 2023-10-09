1835550416a50b680fae69b9e364d622ec495257
davidjia223 <davidjia223@gmail.com>
2023-10-08 18:46:56 -0700

    Training enhanced !

--- trainClassifier.js
+++ trainClassifier.js
@@ -54,81 +54,85 @@ const addAugmentedData = async (sentence, label) => {
   };
 
 // "positive" sentences: these don't seem to involve anything harmful or suspicious
-
-classifier.addDocument(processSentence('Your data will only be used to improve your experience.'), 'positive');
-classifier.addDocument(processSentence('We respect your privacy and will not share your data with third parties without your consent.'), 'positive');
-classifier.addDocument(processSentence('In the event of a dispute, we aim to resolve it amicably with your best interests at heart.'), 'positive');
-classifier.addDocument(processSentence('You have the right to delete your data at any time.'), 'positive');
-classifier.addDocument(processSentence('We protect your data using state-of-the-art encryption technologies.'), 'positive');
-classifier.addDocument(processSentence('We will notify you immediately in case of a data breach.'), 'positive');
-classifier.addDocument(processSentence('Your consent will be requested before we share any of your personal data.'), 'positive');
-classifier.addDocument(processSentence('We comply with all data protection laws and regulations.'), 'positive');
-classifier.addDocument(processSentence('Your information is stored safely and securely.'), 'positive');
-classifier.addDocument(processSentence('We require your explicit permission to share your information.'), 'positive');
-classifier.addDocument(processSentence('All data related disputes are handled in a customer-centric manner.'), 'positive');
-classifier.addDocument(processSentence('User data is encrypted with industry-standard security measures.'), 'positive');
-classifier.addDocument(processSentence('We aim to alert users promptly if any data breach occurs.'), 'positive');
+(async () => {
+await addAugmentedData('Your data will only be used to improve your experience.', 'positive');
+await addAugmentedData('We respect your privacy and will not share your data with third parties without your consent.', 'positive');
+await addAugmentedData('In the event of a dispute, we aim to resolve it amicably with your best interests at heart.', 'positive');
+await addAugmentedData('You have the right to delete your data at any time.', 'positive');
+await addAugmentedData('We protect your data using state-of-the-art encryption technologies.', 'positive');
+await addAugmentedData('We will notify you immediately in case of a data breach.', 'positive');
+await addAugmentedData('Your consent will be requested before we share any of your personal data.', 'positive');
+await addAugmentedData('We comply with all data protection laws and regulations.', 'positive');
+await addAugmentedData('Your information is stored safely and securely.', 'positive');
+await addAugmentedData('We require your explicit permission to share your information.', 'positive');
+await addAugmentedData('All data related disputes are handled in a customer-centric manner.', 'positive');
+await addAugmentedData('User data is encrypted with industry-standard security measures.', 'positive');
+await addAugmentedData('We aim to alert users promptly if any data breach occurs.', 'positive');
 
 // "negative" sentences: these involve data sharing, potential privacy issues, or other matters of concern
-classifier.addDocument(processSentence('Reserves the right, at its sole discretion, to change, modify, add or remove portions of these Terms of Use, at any time.'), 'negative');
-classifier.addDocument(processSentence('We may share your personal information with our affiliates.'), 'negative');
-classifier.addDocument(processSentence('We may change the privacy policy at any time without notice.'), 'negative');
-classifier.addDocument(processSentence('By using our services, you consent to our use of cookies and similar technologies.'), 'negative');
-classifier.addDocument(processSentence('We are not responsible for any third-party links that you access through our service.'), 'negative');
-classifier.addDocument(processSentence('We reserve the right to disclose your personal information if required by law or in response to legal proceedings.'), 'negative');
-classifier.addDocument(processSentence('In the event of a merger or acquisition, your personal information may be transferred to the new entity.'), 'negative');
-classifier.addDocument(processSentence('We may track your browsing habits to serve you targeted advertising.'), 'negative');
-classifier.addDocument(processSentence('We may store your data in servers located in other countries.'), 'negative');
-classifier.addDocument(processSentence('Your data will be stored for an indefinite period even after account deletion.'), 'negative');
-classifier.addDocument(processSentence('Your non-personally identifiable information may be provided to other parties for marketing, advertising, or other uses.'), 'negative');
-classifier.addDocument(processSentence('disputes will be resolved in our sole discretion'), 'negative');
-classifier.addDocument(processSentence('we may disclose your information to our business partners'), 'negative');
-classifier.addDocument(processSentence('We may sell your personal information to third parties for marketing purposes.'), 'negative');
-classifier.addDocument(processSentence('We may change our Terms of Service without giving any notice.'), 'negative');
-classifier.addDocument(processSentence('We hold no responsibility for any data loss or security breaches.'), 'negative');
-classifier.addDocument(processSentence('Your data may be processed in countries with lower data protection standards.'), 'negative');
-classifier.addDocument(processSentence('By using our service, you give up your right to take any disputes to court.'), 'negative');
-classifier.addDocument(processSentence('We have the right to suspend or terminate your account without prior notice or reason.'), 'negative');
-classifier.addDocument(processSentence('You must be at least 18 years old to use our service.'), 'negative');
-classifier.addDocument(processSentence('We may retain your data indefinitely.'), 'negative');
-classifier.addDocument(processSentence('We allow third-party companies to serve ads and/or collect certain anonymous information.'), 'negative');
-classifier.addDocument(processSentence('These terms shall be governed by the laws of the state of California.'), 'negative');
-classifier.addDocument(processSentence('We may terminate your account at any time for any reason.'), 'negative');
-classifier.addDocument(processSentence('We reserve the right to use any user-generated content.'), 'negative');
-classifier.addDocument(processSentence('We disclaim all warranties, express or implied.'), 'negative');
-classifier.addDocument(processSentence('You agree to indemnify us against any claims or legal proceedings.'), 'negative');
-classifier.addDocument(processSentence('We may change or discontinue the service at any time without notice.'), 'negative');
-classifier.addDocument(processSentence('We may use your data for automated decision-making, including profiling.'), 'negative');
-classifier.addDocument(processSentence('We can amend these terms at any time without notifying you.'), 'negative');
-classifier.addDocument(processSentence('Our company is allowed to share your personal details with associated entities.'), 'negative');
-classifier.addDocument(processSentence('We possess the authority to alter the privacy policy unannounced.'), 'negative');
-classifier.addDocument(processSentence('Acceptance of cookies is mandatory to use our services.'), 'negative');
-classifier.addDocument(processSentence('We bear no responsibility for third-party links on our platform.'), 'negative');
-classifier.addDocument(processSentence('We may disclose your data under legal circumstances without your consent.'), 'negative');
-classifier.addDocument(processSentence('In case of a company merger, your personal data may be shared.'), 'negative');
-classifier.addDocument(processSentence('We may monitor your activity for targeted advertising.'), 'negative');
-classifier.addDocument(processSentence('Your personal data might be stored on overseas servers.'), 'negative');
-classifier.addDocument(processSentence('We are allowed to retain your data indefinitely post account termination.'), 'negative');
-classifier.addDocument(processSentence('We can distribute non-personal data for advertising and other purposes.'), 'negative');
-classifier.addDocument(processSentence('Disputes will be resolved as per our company’s discretion.'), 'negative');
-classifier.addDocument(processSentence('We have the authority to share your information with business partners.'), 'negative');
-classifier.addDocument(processSentence('We reserve the right to sell your information for promotional activities.'), 'negative');
-classifier.addDocument(processSentence('We have the freedom to amend our Terms of Service without prior information.'), 'negative');
-classifier.addDocument(processSentence('We assume no responsibility for data loss or security infringements.'), 'negative');
-classifier.addDocument(processSentence('Your data may be managed in regions with less stringent data protection.'), 'negative');
-classifier.addDocument(processSentence('Your consent to use our service relinquishes your rights to legal dispute resolutions.'), 'negative');
-classifier.addDocument(processSentence('We can cease or suspend your account without any prior information or justification.'), 'negative');
-classifier.addDocument(processSentence('Users must be 18 years or older to access our services.'), 'negative');
-classifier.addDocument(processSentence('We permit third-party firms to advertise and gather anonymous data.'), 'negative');
-classifier.addDocument(processSentence('Our terms are governed by Californian law.'), 'negative');
-classifier.addDocument(processSentence('We can terminate your account without providing a reason.'), 'negative');
-classifier.addDocument(processSentence('We reserve the right to use content generated by users.'), 'negative');
-classifier.addDocument(processSentence('All warranties, whether stated or implied, are disclaimed.'), 'negative');
-classifier.addDocument(processSentence('You consent to hold us harmless from any claims or legal proceedings.'), 'negative');
-classifier.addDocument(processSentence('We can discontinue the service without notifying you.'), 'negative');
-classifier.addDocument(processSentence('We have the right to use your data for decision-making and profiling.'), 'negative');
-// ... add more examples
+await addAugmentedData('Reserves the right, at its sole discretion, to change, modify, add or remove portions of these Terms of Use, at any time.', 'negative');
+await addAugmentedData('We may share your personal information with our affiliates.', 'negative');
+await addAugmentedData('We may change the privacy policy at any time without notice.', 'negative');
+await addAugmentedData('By using our services, you consent to our use of cookies and similar technologies.', 'negative');
+await addAugmentedData('We are not responsible for any third-party links that you access through our service.', 'negative');
+await addAugmentedData('We reserve the right to disclose your personal information if required by law or in response to legal proceedings.', 'negative');
+await addAugmentedData('In the event of a merger or acquisition, your personal information may be transferred to the new entity.', 'negative');
+await addAugmentedData('We may track your browsing habits to serve you targeted advertising.', 'negative');
+await addAugmentedData('We may store your data in servers located in other countries.', 'negative');
+await addAugmentedData('Your data will be stored for an indefinite period even after account deletion.', 'negative');
+await addAugmentedData('Your non-personally identifiable information may be provided to other parties for marketing, advertising, or other uses.', 'negative');
+await addAugmentedData('Disputes will be resolved in our sole discretion', 'negative');
+await addAugmentedData('We may disclose your information to our business partners', 'negative');
+await addAugmentedData('We may sell your personal information to third parties for marketing purposes.', 'negative');
+await addAugmentedData('We may change our Terms of Service without giving any notice.', 'negative');
+await addAugmentedData('We hold no responsibility for any data loss or security breaches.', 'negative');
+await addAugmentedData('Your data may be processed in countries with lower data protection standards.', 'negative');
+await addAugmentedData('By using our service, you give up your right to take any disputes to court.', 'negative');
+await addAugmentedData('We have the right to suspend or terminate your account without prior notice or reason.', 'negative');
+await addAugmentedData('You must be at least 18 years old to use our service.', 'negative');
+await addAugmentedData('We may retain your data indefinitely.', 'negative');
+await addAugmentedData('We allow third-party companies to serve ads and/or collect certain anonymous information.', 'negative');
+await addAugmentedData('These terms shall be governed by the laws of the state of California.', 'negative');
+await addAugmentedData('We may terminate your account at any time for any reason.', 'negative');
+await addAugmentedData('We reserve the right to use any user-generated content.', 'negative');
+await addAugmentedData('We disclaim all warranties, express or implied.', 'negative');
+await addAugmentedData('You agree to indemnify us against any claims or legal proceedings.', 'negative');
+await addAugmentedData('We may change or discontinue the service at any time without notice.', 'negative');
+await addAugmentedData('We may use your data for automated decision-making, including profiling.', 'negative');
+await addAugmentedData('We can amend these terms at any time without notifying you.', 'negative');
+await addAugmentedData('Our company is allowed to share your personal details with associated entities.', 'negative');
+await addAugmentedData('We possess the authority to alter the privacy policy unannounced.', 'negative');
+await addAugmentedData('Acceptance of cookies is mandatory to use our services.', 'negative');
+await addAugmentedData('We bear no responsibility for third-party links on our platform.', 'negative');
+await addAugmentedData('We may disclose your data under legal circumstances without your consent.', 'negative');
+await addAugmentedData('In case of a company merger, your personal data may be shared.', 'negative');
+await addAugmentedData('We may monitor your activity for targeted advertising.', 'negative');
+await addAugmentedData('Your personal data might be stored on overseas servers.', 'negative');
+await addAugmentedData('We are allowed to retain your data indefinitely post-account termination.', 'negative');
+await addAugmentedData('We can distribute non-personal data for advertising and other purposes.', 'negative');
+await addAugmentedData('Disputes will be resolved as per our company’s discretion.', 'negative');
+await addAugmentedData('We have the authority to share your information with business partners.', 'negative');
+await addAugmentedData('We reserve the right to sell your information for promotional activities.', 'negative');
+await addAugmentedData('We have the freedom to amend our Terms of Service without prior information.', 'negative');
+await addAugmentedData('We assume no responsibility for data loss or security infringements.', 'negative');
+await addAugmentedData('Your data may be managed in regions with less stringent data protection.', 'negative');
+await addAugmentedData('Your consent to use our service relinquishes your rights to legal dispute resolutions.', 'negative');
+await addAugmentedData('We can cease or suspend your account without any prior information or justification.', 'negative');
+await addAugmentedData('Users must be 18 years or older to access our services.', 'negative');
+await addAugmentedData('We permit third-party firms to advertise and gather anonymous data.', 'negative');
+await addAugmentedData('Our terms are governed by Californian law.', 'negative');
+await addAugmentedData('We can terminate your account without providing a reason.', 'negative');
+await addAugmentedData('We reserve the right to use content generated by users.', 'negative');
+await addAugmentedData('All warranties, whether stated or implied, are disclaimed.', 'negative');
+await addAugmentedData('You consent to hold us harmless from any claims or legal proceedings.', 'negative');
+await addAugmentedData('We can discontinue the service without notifying you.', 'negative');
+await addAugmentedData('We have the right to use your data for decision-making and profiling.', 'negative');
+
 classifier.train();
+})();
+
+// ... add more examples
+
 module.exports = {
     classifier,
     processSentence
